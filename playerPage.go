c3f25ac3e8609fc7bac35e16f2c2bbdd0cb46392
MathiasB <git@denbeke.be>
2014-03-28 15:53:08 +0100

    Parsing all information of player

--- playerPage.go
+++ playerPage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func parsePlayer(url string) (int, error) {
@@ -10,33 +11,37 @@ func parsePlayer(url string) (int, error) {
 		log.Printf("could not parse referee %s: %s", url, err)
 		return 0, err
 	}
-
-	infoTag := d.Selection.Find(".content .first dd")
-
-	firstname, err := infoTag.Html()
-	if err != nil {
-		log.Printf("Could not find firstname for player %s\n", url)
-		return 0, err
-	}
-
-	infoTag = infoTag.Next().Next()
-	lastname, err := infoTag.Html()
-	if err != nil {
-		log.Printf("Could not find lastname for player %s\n", url)
-		return 0, err
-	}
-
-	infoTag = infoTag.Next().Next()
-	country, err := infoTag.Html()
-	if err != nil {
-		log.Printf("Could not find country for player %s\n", url)
-		return 0, err
-	}
-
-	// TODO: Add player, country
-	_ = firstname
-	_ = lastname
-	_ = country
-
+		
+	
+	//Info contains the attributes of the player
+	//e.g. info['Position'] = "Defender"
+	info := map[string]string{}
+	
+	
+	d.Find(".content .first dl").Children().EachWithBreak(func(i int, s *goquery.Selection) bool {
+		
+		//We only check the even elements (= dd elements)
+		if(i % 2 == 0) {
+		
+			key := s.Text()
+			valueSelection := s.Next()
+			
+			//Check if the given dd element exists for this dt element
+			if(valueSelection.Length() < 1) {
+				log.Printf("Could not find metadata for player (unmatching dt/dd) %s\n", url)
+				return false;
+			}
+			
+			value := valueSelection.Text()
+			info[key] = value
+			
+		}
+		
+		return true;
+	})
+	
+	
+	_ = info
+	
 	return 1, nil
 }
